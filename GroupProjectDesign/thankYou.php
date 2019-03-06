34baf3c694d1f523cfe05298eb166b6e0f4b9c5c
Timo Hartwig <timothy.m.hartwig@gmail.com>
2019-03-06 13:14:10 -0600

    Changed thankYou.html to thankYou.php

--- GroupProjectDesign/thankYou.php
+++ GroupProjectDesign/thankYou.php
@@ -7,6 +7,16 @@
 </head>
 
 <body>
+  <?php
+    function sanitizeString($field) {
+
+      $output = filter_input(INPUT_POST, $field, FILTER_SANITIZE_STRING);
+
+      return $output;
+    }
+
+    $name = sanitizeString($_POST["uName"]);
+  ?>
   <div id="wrapper">
     <header class="panel">
       <img class="imagelogo" height="100px" src="images/GAMEBOOk_image_logo.png">
@@ -27,10 +37,10 @@
     </div>
 
     <main>
-      <content>
-        <h2>Thank You, <?= $_POST["uName"] ?>.</h2>
+      <div>
+        <h2>Thank You, <?= $name ?>.</h2>
         <p>We have recieved you comments and will get back to you soon.</p>
-      </content>
+      </div>
     </main>
 
     <footer id="mainFooter" class="panel">
