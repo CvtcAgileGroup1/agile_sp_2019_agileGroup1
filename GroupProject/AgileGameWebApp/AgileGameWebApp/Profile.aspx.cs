359cb618c2665c13769c6de8b07981b253c73653
CrusherTechnologies <crusher9051@gmail.com>
2019-03-08 19:19:17 -0600

    Revert "Added Profile Pages"
    This reverts commit b4d9cdf3ff5127509a77465f9035e8a534b9b8a0.

--- GroupProject/AgileGameWebApp/AgileGameWebApp/Profile.aspx.cs
+++ GroupProject/AgileGameWebApp/AgileGameWebApp/Profile.aspx.cs
@@ -9,38 +9,8 @@ namespace AgileGameWebApp
 {
     public partial class Profile : System.Web.UI.Page
     {
-        MySql.Data.MySqlClient.MySqlConnection conn;
-        //MySql.Data.MySqlClient.MySqlCommand cmd;
-        UserClass user = new UserClass();
-        String querystring = "";
         protected void Page_Load(object sender, EventArgs e)
         {
-            conn = connectionString();
-            conn.Open();
-            querystring = String.Format("SELECT userID uName,fName,lName,email,age,city,state,zip,bio FROM user WHERE user.userID = '{0}'", 2);
-            MySql.Data.MySqlClient.MySqlCommand cmd = new MySql.Data.MySqlClient.MySqlCommand(querystring, conn);
-            MySql.Data.MySqlClient.MySqlDataReader dataReader = cmd.ExecuteReader();
-            if (dataReader.Read())
-            {
-                Label1.Text = dataReader[0].ToString();
-                Label1.Text = dataReader[1].ToString();
-                Label1.Text = dataReader[2].ToString();
-                Label1.Text = dataReader[3].ToString();
-                Label1.Text = dataReader[4].ToString();
-                Label1.Text = dataReader[5].ToString();
-                Label1.Text = dataReader[6].ToString();
-                Label1.Text = dataReader[7].ToString();
-                Label1.Text = dataReader[8].ToString();
-                
-            }
-            
-        }
-
-        private MySql.Data.MySqlClient.MySqlConnection connectionString()
-        {
-            String connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebAppConnString"].ToString();
-
-            return new MySql.Data.MySqlClient.MySqlConnection(connString);
 
         }
     }
